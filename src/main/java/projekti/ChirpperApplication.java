68d59693f25f63b70f9359f4ce36283915df1c26
k0psutin <mr.gravez@gmail.com>
2019-11-27 20:04:06 +0200

    .

--- src/main/java/projekti/ChirpperApplication.java
+++ src/main/java/projekti/ChirpperApplication.java
@@ -1,9 +1,11 @@
 package projekti;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ChirpperApplication {
 
     public static void main(String[] args) {
