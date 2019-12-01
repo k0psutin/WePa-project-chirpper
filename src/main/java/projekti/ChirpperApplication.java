52ee161ae6dc3cb551915666b62b6eded14a501c
k0psutin <mr.gravez@gmail.com>
2019-12-02 01:38:44 +0200

    timezone fix

--- src/main/java/projekti/ChirpperApplication.java
+++ src/main/java/projekti/ChirpperApplication.java
@@ -13,7 +13,7 @@ public class ChirpperApplication {
 
     @PostConstruct
     public void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+        TimeZone.setDefault(TimeZone.getTimeZone("Finland/Helsinki"));
     }
 
     public static void main(String[] args) {
